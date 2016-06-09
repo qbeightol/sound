638e8a488938d6064019a130ab9cb9aa0159cba6
Quinn Beightol <qeb2@cornell.edu>
2016-06-09 08:22:17 -0500

    got docs working

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,20 @@
 extern crate byteorder;
 extern crate num;
 
-mod io {
-    mod aif;
+/// Modules for manipulating sound-related files
+pub mod io {
+    /// A module for creating single-channel .aif files from a vector of 32-bit samples
+    pub mod aif;
 }
 
+/// Libraries for generating sounds
 mod sound {
+    /// A library for generating primitive waves
     mod wave {
 
     }
+
+    /// Ways to combine sounds into new sounds
     mod combinator {
 
     }
