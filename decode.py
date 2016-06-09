1458a8beac0dea4e7592e85d14d09be96bf55a6f
Quinn Beightol <qeb2@cornell.edu>
2016-06-08 22:00:33 -0500

    created a script for displaying the byte content of files

---
+++ decode.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+'''
+A script that prints the contents of a file by providing each 
+'''
+
+import os
+import sys
+
+def script():
+    target = sys.argv[1] # what we should decode
+    with open(target, 'r') as target_file: 
+        for byte in target_file.read():
+            print(str(ord(byte)) + "\t(" + byte + ")");
+        # print(target_file.read())
+
+if __name__ == '__main__':
+    script()
