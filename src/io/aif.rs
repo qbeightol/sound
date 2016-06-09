b6263a899cbdb0ddf32fbe2dab0174ac179668c1
Quinn Beightol <qeb2@cornell.edu>
2016-06-09 08:22:09 -0500

    more work on serialization

--- src/io/aif.rs
+++ src/io/aif.rs
@@ -1,21 +1,27 @@
+/// DOCUMENTATION
+
 // extern crate byteorder;
 use std::io::SeekFrom;
 use std::io::prelude::*;
 use std::fs::File;
 use byteorder::{ByteOrder, BigEndian};
 
+const NUM_CHANNELS : u32 = 1;
+const SAMPLE_SIZE_BITS : u32 = 32;
+const SAMPLE_SIZE_BYTES : u32 = 32 / 8;
 
 /// A single-channel .aif audio file, where each sample is 32 bits.
-struct Aif {
+pub struct Aif {
     audio : Vec<u32>
 }
 
 impl Aif {
 
-    /// returns the size of the form chunk's data (measured in bytes). The form chunk's data
-    /// includes both the form type (i.e. AIFF) and the nested chunks (in this case, just the
-    /// common chunk and the data chunk).
-    fn form_chunk_size(&self) -> u32 {
+    /// returns the size of the form chunk's data measured in bytes.
+    ///
+    /// The form chunk's data includes both the form type (i.e. AIFF) and the nested chunks.
+    /// Currently, the nested chunks are a common chunk and a sound chunk.
+    pub fn form_chunk_size(&self) -> u32 {
         return 12 // ??? (I used to assume that this "FORM", form size, and "AIFF", but that
                   // contradicts the spec---those first 2 values wouldn't be included)
             + 8   // "COMM", size of common chunk
@@ -25,22 +31,48 @@ impl Aif {
             + 8;   // offset size, block size ?
     }
 
-    /// appends the a serialized version of itself to file.
-    fn write_to_file(&self, file : &mut File) {
-        // write form chunk
-        file.write_all(b"FORM");
+    /// appends a serialized version of the aif file to `file`.
+    pub fn write_to_file(&self, file : &mut File) {
 
-        let mut total_size_as_bytes : [u8; 4] = [0; 4];
-        // it looks the aif standard always uses big endian representations
-        BigEndian::write_u32(&mut total_size_as_bytes, self.form_chunk_size());
-        file.write_all(&total_size_as_bytes);
+        let mut byte_buffer : [u8; 4] = [0; 4];
+
+        // write form chunk ///////////////////////////////
+        file.write_all(b"FORM");
+        // it might seem like we should use native endian-ness, but it looks the aif standard
+        // always uses big-endian representations
+        BigEndian::write_u32(&mut byte_buffer, self.form_chunk_size());
+        file.write_all(&byte_buffer);
         file.write_all(b"AIFF");
 
+        // write common chunk /////////////////////////////
+        file.write_all(b"COMM");
+        // size of common chunk
+        BigEndian::write_u32(&mut byte_buffer, 18);
+        file.write_all(&byte_buffer);
+        BigEndian::write_u32(&mut byte_buffer, NUM_CHANNELS);
+        file.write_all(&byte_buffer);
+        BigEndian::write_u32(&mut byte_buffer, self.audio.len() as u32);
+        file.write_all(&byte_buffer);
+        BigEndian::write_u32(&mut byte_buffer, SAMPLE_SIZE_BITS);
+        file.write_all(&byte_buffer);
+        // this is the sample rate (44.1k) using an 'extended' format (I think this is an
+        // Apple-developed float representation)
+        file.write_all(&[0x40, 0xe, 0xac, 0x44, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0]);
+
+        // write sound chunk //////////////////////////////
+        file.write_all(b"SSND");
+        // size of sound chunk
+        BigEndian::write_u32(&mut byte_buffer, SAMPLE_SIZE_BYTES * self.audio.len() as u32 + 8);
+        file.write_all(&byte_buffer);
+        // offset size
+        BigEndian::write_u32(&mut byte_buffer, 0);
+        file.write_all(&byte_buffer);
+        // block size
+        BigEndian::write_u32(&mut byte_buffer, 0);
+        file.write_all(&byte_buffer);
 
 
-        // write common chunk
 
-        // write sound chunk
     }
 }
 
